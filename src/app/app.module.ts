ca8e52b918660ec9b2884b84793a9b0debc5405a
j3ank <eugenia.stempel@icloud.com>
2021-07-09 22:33:27 +0200

    add default components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AuthComponent } from './auth/auth.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactComponent } from './contact/contact.component';
+import { ImprintComponent } from './imprint/imprint.component';
+import { ProductComponent } from './product/product.component';
+import { LogoutComponent } from './logout/logout.component';
+import { ProfileComponent } from './profile/profile.component';
+import { OrderComponent } from './order/order.component';
+import { LicenceComponent } from './licence/licence.component';
+import { GroupComponent } from './group/group.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AuthComponent,
+    AboutUsComponent,
+    ContactComponent,
+    ImprintComponent,
+    ProductComponent,
+    LogoutComponent,
+    ProfileComponent,
+    OrderComponent,
+    LicenceComponent,
+    GroupComponent
   ],
   imports: [
     BrowserModule,
