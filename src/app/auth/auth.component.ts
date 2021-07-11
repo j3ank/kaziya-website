f06d5574458290b600e841be035dec10b42f6292
j3ank <eugenia.stempel@icloud.com>
2021-07-11 21:44:04 +0200

    auth

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { Errors } from '../core';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.scss']
+  styleUrls: ['./auth.component.scss'],
 })
 export class AuthComponent implements OnInit {
+  errors: Errors = { errors: {} };
+  isSubmitting = false;
+  authForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private fb: FormBuilder
+  ) {
+    this.authForm = this.fb.group({
+      userName: ['', Validators.required],
+      password: ['', Validators.required],
+    });
   }
 
+  ngOnInit(): void {}
+
+  submitForm() {
+    this.isSubmitting = true;
+    this.errors = { errors: {} };
+
+    const credentials = this.authForm.value;
+
+    console.log(credentials);
+
+    debugger;
+
+    this.router.navigateByUrl('/');
+  }
 }
